e08780d10557b81cb18f4ecc730231f3d69a5893
Kranberri <56488228+kran27@users.noreply.github.com>
2022-02-25 01:51:52 -0800

    Default to dark mode
    Forgot to do this earlier.

--- DarkUI2/Config/ThemeProvider.cs
+++ DarkUI2/Config/ThemeProvider.cs
@@ -5,7 +5,7 @@ namespace DarkUI2.Config
     public class ThemeProvider
     {
         private static ITheme theme;
-        public static int LightMode = (int) Registry.GetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", "SystemUsesLightTheme", 1);
+        public static int LightMode = (int) Registry.GetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", "SystemUsesLightTheme", 0);
         public static ITheme Theme
         {
             get
