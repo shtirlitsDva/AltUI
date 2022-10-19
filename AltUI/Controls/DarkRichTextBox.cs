49253e4e2b73984ab6639b30a6e489729479f92f
Kran <56488228+kran27@users.noreply.github.com>
2022-10-19 16:06:41 -0700

    minor update

--- AltUI/Controls/DarkRichTextBox.cs
+++ AltUI/Controls/DarkRichTextBox.cs
@@ -1,12 +1,20 @@
 using AltUI.Config;
 using System;
 using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace AltUI.Controls
 {
     public class DarkRichTextBox : RichTextBox
     {
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public new bool HasFocus
+        {
+            get { return Focused; }
+        }
         public DarkRichTextBox()
         {
             BackColor = ThemeProvider.Theme.Colors.GreyBackground;
